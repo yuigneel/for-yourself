ecbf7908f214f52b26182bdb17376006eb2e5181
yulgnier <yu_lgnier@outlook.com>
2026-03-30 10:22:47 +0800

    rename(config): 将应用配置从bangumi更改为your
    - 修改服务器端口配置变量从${bangumi.server.port}到${your.server.port}
    - 修改时区配置变量从${bangumi.timezone}到${your.timezone}
    - 更新数据源URL配置从${bangumi.datasource.*}到${your.datasource.*}
    - 修改数据源用户名密码配置从${bangumi.datasource.*}到${your.datasource.*}
    - 更改HikariCP连接池名称从BangumiHikariPool到yourHikariPool
    - 更新Knife4j接口文档标题从"动漫追番管理用户接口文档"到"For-Yourself普通用户接口文档"
    - 修改API规则资源配置包路径从com.yulgnier.user.controller到com.yulgnier.center.common.user.controller
    - 新增CenterCommonUserApplication启动类

---
+++ center-user-web/common-user-service/src/main/java/com/yulgnier/center/common/user/CenterCommonUserApplication.java
@@ -0,0 +1,10 @@
+package com.yulgnier.center.common.user;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CenterCommonUserApplication {
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(CenterCommonUserApplication.class, args);
+    }
+}
